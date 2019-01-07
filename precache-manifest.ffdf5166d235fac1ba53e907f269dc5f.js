1ac6d11bbd2283572c26413fbb0003e86fafbaeb
RyanPai <garyetofu@gmail.com>
2019-01-07 21:11:34 +0800

    feat: localhost done

---
+++ precache-manifest.ffdf5166d235fac1ba53e907f269dc5f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "50cfb2cb7a91203e2965",
+    "url": "/static/js/main.50cfb2cb.chunk.js"
+  },
+  {
+    "revision": "9d0f5ad792b0acc79c0a",
+    "url": "/static/js/1.9d0f5ad7.chunk.js"
+  },
+  {
+    "revision": "50cfb2cb7a91203e2965",
+    "url": "/static/css/main.f65c0092.chunk.css"
+  },
+  {
+    "revision": "7c116e1eedc57ce423b8c37bbbeef829",
+    "url": "/index.html"
+  }
+];
