8b11c58ba718ed088e8ee5a2943db7fc1f67d0b9
RyanPai <garyetofu@gmail.com>
2019-01-08 20:54:22 +0800

    fix: not fuond

--- precache-manifest.ffdf5166d235fac1ba53e907f269dc5f.js
+++ precache-manifest.ffdf5166d235fac1ba53e907f269dc5f.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/static/js/runtime~main.229c360f.js"
   },
   {
-    "revision": "50cfb2cb7a91203e2965",
-    "url": "/static/js/main.50cfb2cb.chunk.js"
+    "revision": "6e6d8c1a94991fbbba98",
+    "url": "/static/js/main.6e6d8c1a.chunk.js"
   },
   {
-    "revision": "9d0f5ad792b0acc79c0a",
-    "url": "/static/js/1.9d0f5ad7.chunk.js"
+    "revision": "d42f50c5fdd0cc5f15d0",
+    "url": "/static/js/1.d42f50c5.chunk.js"
   },
   {
-    "revision": "50cfb2cb7a91203e2965",
+    "revision": "6e6d8c1a94991fbbba98",
     "url": "/static/css/main.f65c0092.chunk.css"
   },
   {
-    "revision": "7c116e1eedc57ce423b8c37bbbeef829",
+    "revision": "c2af87fb219011661b529bc5ed25ee2c",
     "url": "/index.html"
   }
 ];
